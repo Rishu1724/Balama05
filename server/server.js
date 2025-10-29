11e415fe3ec5a89ea68c41b841016a33e106aafd
Rishu1724 <rishuraj1721zo@gmail.com>
2025-10-29 09:31:49 +0530

    data set addede to the Ml and workflow

--- server/server.js
+++ server/server.js
@@ -50,7 +50,7 @@ connectToMongo().then(() => {
 
 // CORS configuration
 const corsOptions = {
-  origin: "http://localhost:3000",
+  origin: ["http://localhost:3000", "http://localhost:3002", "http://127.0.0.1:3000", "http://127.0.0.1:3002"],
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Origin", "X-Requested-With", "Content-Type", "Accept", "Authorization"],
   credentials: true
